cda5cff3cf71001d5fb24427bc0884a8d05c45c7
Grzegorz <grzegorzmickowski@MacBook-Air-Karina.local>
2017-01-19 13:26:44 +0100

    Bytes through socket functionality

--- teslaCharger/teslaCharger/ViewController.swift
+++ teslaCharger/teslaCharger/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func sendDataAndRecieveAnwser(inputData: String) -> String{
+    func sendStringAndRecieveAnwser(inputData: String) -> String{
         switch client.send(string: inputData ) {
             case .success:
                 guard let data = client.read(1024*10) else { return "ERR"}
@@ -45,14 +45,51 @@ class ViewController: UIViewController {
     }
     
     
+    
+    func sendDataAndRecieveAnwser(inpuData: Data, action: String){
+        switch client.send(data: inpuData) {
+            case .success:
+                guard let data = client.read(1024*10) else { break}
+                
+                let funcNumber = data[0]
+                let addressArr = Data(data[1...2])
+                let address = UInt16(bigEndian: addressArr.withUnsafeBytes {$0.pointee})
+                let valueArr = Data(data[3...4])
+                let value = UInt16(bigEndian: valueArr.withUnsafeBytes {$0.pointee})
+                
+                switch action{
+                    case "ON":
+                        chargerStatusLabel.text = "Charger status: ONLINE"
+                    case "OFF":
+                        chargerStatusLabel.text = "Charger status: OFFLINE"
+                    default:
+                        chargerStatusLabel.text = "Charger status: ERROR"
+                }
+                
+                print(data)
+                print("Funkcja: \(funcNumber), Adres: \(address), Wartość: \(value)")
+                
+            case .failure(let error):
+                print(error)
+            }
+    }
+    
+    
     @IBOutlet weak var chargerStatusLabel: UILabel!
     
     @IBAction func onButtonPressed(_ sender: Any) {
-        chargerStatusLabel.text = "Charger status: " + sendDataAndRecieveAnwser(inputData: "ON")
+        let request :[UInt8] = [0x05,0x01,0x90,0xFF,0x00]
+        let requestData = Data(request)
+        
+        sendDataAndRecieveAnwser(inpuData: requestData, action: "ON")
+        
     }
     
     @IBAction func offButtonPressed(_ sender: Any) {
-        chargerStatusLabel.text = "Charger status: " + sendDataAndRecieveAnwser(inputData: "OFF")
+        let request :[UInt8] = [0x05,0x01,0x90,0x00,0x00]
+        let requestData = Data(request)
+        
+        sendDataAndRecieveAnwser(inpuData: requestData, action: "OFF")
     }
 
     override func viewDidLoad() {
